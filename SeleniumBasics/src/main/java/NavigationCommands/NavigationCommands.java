b78ea33cfd3b4d3540ab49cc5fcb969157cfcd26
MyPc <MyPc@DESKTOP-UMRLRJO>
2024-07-22 22:22:14 +0530

    1

--- SeleniumBasics/src/main/java/NavigationCommands/NavigationCommands.java
+++ SeleniumBasics/src/main/java/NavigationCommands/NavigationCommands.java
@@ -7,7 +7,7 @@ public class NavigationCommands {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\MyPc\\Downloads\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\MyPc\\Downloads\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.javatpoint.com/method-in-java");
 		driver.manage().window().maximize();
